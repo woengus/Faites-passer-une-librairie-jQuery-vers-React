ec80c7454abb0002631fc5e16ff62ce2e435e07f
woengus <olivierbiard@woengus.com>
2023-06-19 16:04:21 +0200

    commentaire  modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,25 @@
+/**
+ * Composant Modal pour afficher une superposition modale avec un message et un bouton de fermeture.
+ */
 import React, { useState } from "react";
 
+/**
+ * Représente une superposition modale.
+ * @returns {JSX.Element} La superposition modale.
+ */
 const Modal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  /**
+   * Ouvre la superposition modale.
+   */
   const openModal = () => {
     setIsModalOpen(true);
   };
 
+  /**
+   * Ferme la superposition modale.
+   */
   const closeModal = () => {
     setIsModalOpen(false);
   };
@@ -18,7 +31,7 @@ const Modal = () => {
       {isModalOpen && (
         <div className="modal-overlay">
           <div className="modal">
-            <p>Employee Created!</p>
+            <p>Employee created !</p>
             <button onClick={closeModal}>Close</button>
           </div>
         </div>
@@ -26,48 +39,48 @@ const Modal = () => {
 
       <style>
         {`
-          .modal-overlay {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: rgba(0, 0, 0, 0.5);
-            z-index: 9999;
-          }
-          
-          .modal {
-            width: 180px;
-            height: 50px;
-            background-color: #f0f0f0;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            padding: 20px;
-            background-color: aquamarine;
-          }
-          
-          .modal p {
-            margin: 0;
-          }
-          
-          .modal button {
-            background-color: transparent;
-            border: none;
-            cursor: pointer;
-            padding: 0;
-            color: #333;
-            font-weight: bold;
-            font-size: 18px;
-            margin-top: 10px;
-          }
-        `}
+           .modal-overlay {
+             position: fixed;
+             top: 0;
+             left: 0;
+             width: 100%;
+             height: 100%;
+             display: flex;
+             justify-content: center;
+             align-items: center;
+             background-color: rgba(0, 0, 0, 0.5);
+             z-index: 9999;
+           }
+           
+           .modal {
+             width: 180px;
+             height: 50px;
+             background-color: #f0f0f0;
+             border: 1px solid #ccc;
+             border-radius: 4px;
+             display: flex;
+             flex-direction: column;
+             justify-content: center;
+             align-items: center;
+             padding: 20px;
+             background-color: aquamarine;
+           }
+           
+           .modal p {
+             margin: 0;
+           }
+           
+           .modal button {
+             background-color: transparent;
+             border: none;
+             cursor: pointer;
+             padding: 0;
+             color: #333;
+             font-weight: bold;
+             font-size: 18px;
+             margin-top: 10px;
+           }
+         `}
       </style>
     </>
   );
